2dfbf47dccab9e454bed57aa4812e96159cfbe79
Akromjon <152626511+RustamovAkrom@users.noreply.github.com>
2024-10-20 13:17:11 +0500

    add github acction (lintcode.yml)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pdfplumber
-import re 
+import re
 import json
 
 
@@ -12,41 +12,40 @@ def extract_structure_from_pdf(pdf_file_path):
         start_page_number = 0
         end_page_number = 11
 
-        for page in pdf.pages[start_page_number: end_page_number]:
+        for page in pdf.pages[start_page_number:end_page_number]:
             text = page.extract_text()
 
-            lines = text.split('\n')
+            lines = text.split("\n")
 
             for line in lines:
                 line = line.strip()
 
-                chapter_match = re.match(r'(\d+)\.\s*(.+)', line)
-                
+                chapter_match = re.match(r"(\d+)\.\s*(.+)", line)
+
                 if chapter_match:
                     chapter_number = chapter_match.group(1).strip()
                     chapter_title = chapter_match.group(2).strip()
-                    structure[chapter_number] = {
-                        "title": chapter_title,
-                        "sections": {}
-                    }
+                    structure[chapter_number] = {"title": chapter_title, "sections": {}}
                     current_chapter = chapter_number
                     current_section = None
 
-                section_match = re.match(r'(\d+\.\d+)\s*(.+)', line)
+                section_match = re.match(r"(\d+\.\d+)\s*(.+)", line)
 
                 if current_chapter and section_match:
                     section_number = section_match.group(1).strip()
                     section_title = section_match.group(2).strip()
                     structure[current_chapter]["sections"][section_number] = {
                         "title": section_title,
-                        "subsections": {}
+                        "subsections": {},
                     }
                     current_section = section_number
-                subsection_match = re.match(r'(\d+\.\d+\.\d+)\s*(.+)', line)
+                subsection_match = re.match(r"(\d+\.\d+\.\d+)\s*(.+)", line)
                 if current_section and subsection_match:
                     subsection_number = subsection_match.group(1).strip()
                     subsection_title = subsection_match.group(2).strip()
-                    structure[current_chapter]["sections"][current_section]["subsections"][subsection_number] = {
+                    structure[current_chapter]["sections"][current_section][
+                        "subsections"
+                    ][subsection_number] = {
                         "title": subsection_title,
                     }
 
@@ -56,22 +55,24 @@ def extract_structure_from_pdf(pdf_file_path):
 def delete_dublicate(structure: dict):
     for chapter in structure.values():
         for section in chapter["sections"].values():
-            section['title'] = re.sub(r'[^\w\s.,-]', '', section["title"]).strip()
+            section["title"] = re.sub(r"[^\w\s.,-]", "", section["title"]).strip()
 
-            for subsection in section['subsections'].values():
-                subsection['title'] = re.sub(r'[^\w\s.,-]', '', subsection["title"]).strip()
+            for subsection in section["subsections"].values():
+                subsection["title"] = re.sub(
+                    r"[^\w\s.,-]", "", subsection["title"]
+                ).strip()
 
     return structure
 
 
 def save_data_to_json(structure, path):
-    with open(path, 'w', encoding='utf-8') as file:
+    with open(path, "w", encoding="utf-8") as file:
         json.dump(structure, file, ensure_ascii=False, indent=4)
 
 
 def main():
-    pdf_file = 'data/structure.pdf'
-    json_file = 'structure.json'
+    pdf_file = "data/structure.pdf"
+    json_file = "structure.json"
 
     data = extract_structure_from_pdf(pdf_file)
     data = delete_dublicate(data)
@@ -80,5 +81,5 @@ def main():
     save_data_to_json(data, json_file)
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     main()
